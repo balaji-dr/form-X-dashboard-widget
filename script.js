1413ebfc8a2b96fb66a40af6a6e2a4952c4a826b
Balaji D R <drbalaji97@gmail.com>
2018-06-09 18:18:09 +0530

    added modal view

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ function print_sub_data(sub_data) {
 function print_single_sub_data(single_data){
     var data = JSON.parse(single_data.submissionData)
         for (var key in data){
-            document.getElementById("app").innerHTML += key + "  " + data[key] + "<br>"
+            document.getElementById("content").innerHTML += key + "  " + data[key] + "<br>"
         }
 }
 
 function print_visit_data(visit_data){
     for (var key in visit_data){
-        document.getElementById("app").innerHTML += key + "  " + visit_data[key] + "<br>"
+        document.getElementById("content").innerHTML += key + "  " + visit_data[key] + "<br>"
     }
 }
 
@@ -55,33 +55,33 @@ async function get_single_sub(id){
         })
         var json_response = await response.json()
         console.log(json_response.data);
-        document.getElementById("app").innerHTML = ""
+        document.getElementById("content").innerHTML = ""
         print_single_sub_data(json_response.data)
-        document.getElementById("app").innerHTML += "Time stamp: " + json_response.data.timestamp + "<br>";
+        document.getElementById("content").innerHTML += "Time stamp: " + json_response.data.timestamp + "<br>";
 
-        document.getElementById("app").innerHTML += "<b>First Visit Details:</b> <br> "
+        document.getElementById("content").innerHTML += "<b>First Visit Details:</b> <br> "
         print_visit_data(json_response.data.analyticalData.submitter.firstVisit)
-        document.getElementById("app").innerHTML += "<b>Last Visit Details:</b> <br> "
+        document.getElementById("content").innerHTML += "<b>Last Visit Details:</b> <br> "
         print_visit_data(json_response.data.analyticalData.submitter.lastVisit)
 
-        document.getElementById("app").innerHTML += "<br><br>";
+        document.getElementById("content").innerHTML += "<br><br>";
 
 
-        document.getElementById("app").innerHTML += "Number of Visits : "+json_response.data.analyticalData.submitter.numberOfVisits + "<br>";
-        document.getElementById("app").innerHTML += "Time spent in website : " + json_response.data.analyticalData.submitter.totalTimeSpentInWebsite + "<br>";
+        document.getElementById("content").innerHTML += "Number of Visits : "+json_response.data.analyticalData.submitter.numberOfVisits + "<br>";
+        document.getElementById("content").innerHTML += "Time spent in website : " + json_response.data.analyticalData.submitter.totalTimeSpentInWebsite + "<br>";
 
 
 
-        var btn = document.createElement('button');
-            btn.innerText = "Back";
-            btn.id = "back";
-        var container = document.getElementById("app");
-            container.appendChild(btn);  
-        var element = document.getElementById("back");
-        element.onclick = function () {
-            document.getElementById("app").innerHTML = "";
-                get_all_submissions();
-            }
+        // var btn = document.createElement('button');
+        //     btn.innerText = "Back";
+        //     btn.id = "back";
+        // var container = document.getElementById("app");
+        //     container.appendChild(btn);  
+        // var element = document.getElementById("back");
+        // element.onclick = function () {
+        //     document.getElementById("app").innerHTML = "";
+        //         get_all_submissions();
+        //     }
         return json_response
     } catch (e) {
         alert('Error!')
@@ -92,8 +92,7 @@ async function get_all_submissions() {
    let resp = await login()
     console.log(token)
     var d = document.getElementById("app")
-    d.innerHTML += "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'>Open Modal</button>" 
-    d.innerHTML += "<div id='myModal' class='modal fade' role='dialog'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'>&times;</button><h4 class='modal-title'>Modal Header</h4></div><div class='modal-body'><p>Some text in the modal.</p></div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Close</button></div></div></div></div><br><br>";
+    d.innerHTML += "<div id='myModal' class='modal fade' role='dialog'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'>&times;</button><h4 class='modal-title'>Modal Header</h4></div><div class='modal-body'><p id='content'></p></div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Close</button></div></div></div></div>";
 
     // try {
         var response = await
@@ -114,6 +113,9 @@ async function get_all_submissions() {
             var btn = document.createElement('button');
             btn.innerText = "View";
             btn.id = sub_data[i].id;
+            btn.className += "btn btn-primary"
+            btn.setAttribute("data-target", "#myModal");
+            btn.setAttribute("data-toggle","modal");
             print_sub_data(sub_data[i]);
             var container = document.getElementById("app");
             container.appendChild(btn);
