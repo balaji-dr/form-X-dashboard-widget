86cc54818087c0b4a88539b4ca7b88551ae330c0
Balaji D R <drbalaji97@gmail.com>
2018-06-14 21:29:00 +0530

    added project api

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 var token = null;
 var sub_data = null;
 var sub_id = null;
-
 async function login() {
     try {
     
         var response = await fetch('https://api.formx.stream/api/token', {
             body:JSON.stringify( {
-                username: "drbalaji97@gmail.com",
-                password: "spiderman"
+                username: xusername,
+                password: xpassword
             }),
             method: 'POST',
             headers:{
@@ -63,8 +62,8 @@ async function get_single_sub(id){
                 'Content-Type': 'application/json'
             }
         })
-        var json_response = await response.json()
-        console.log(json_response.data);        
+        
+        var json_response = await response.json()    
         document.getElementById("content").innerHTML = ""
         print_single_sub_data(json_response.data)
         var datetime = new Date(json_response.data.timestamp * 1000);
@@ -82,19 +81,61 @@ async function get_single_sub(id){
         document.getElementById("content").innerHTML += " <b> Time spent in website : </b>" + round(json_response.data.analyticalData.submitter.totalTimeSpentInWebsite,2)  + "<br>";
         return json_response
     } catch (e) {
-        alert('Error!')
+        alert(e)
     }
 }
 
-async function get_all_submissions() {
+async function select_project() {
    let resp = await login()
-    console.log(token)
     var d = document.getElementById("app")
     d.innerHTML += "<div id='myModal' class='modal fade' role='dialog'><div class='modal-dialog'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal'>&times;</button><h4 class='modal-title'>Submission information</h4></div><div class='modal-body'><p id='content'></p></div><div class='modal-footer'><button type='button' class='btn btn-default' data-dismiss='modal'>Close</button></div></div></div></div>";
+    try{
+        var p_resp = await
+        fetch('https://api.formx.stream/api/forms/'+ project_id,{
+            method: 'GET',
+            headers: {
+                'Authorization': 'jwt ' + token,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        project_response = await p_resp.json();
+        var d = document.getElementById("app")
+        var select = document.createElement("select");
+
+        select.id = "projects";
+        select.required = "required";
+        var option = document.createElement("option");
+        option.value = "";
+        option.text = "Choose your form";
+        option.selected = "selected";
+        option.disabled = "disabled";
+        option.hidden = "hidden";
+        select.appendChild(option);
+        for (i=0; i<project_response.data.length; i++){
+            var option = document.createElement("option");
+            option.value = project_response.data[i].id;
+            option.text = project_response.data[i].name;
+            select.appendChild(option);
+        }
+        d.appendChild(select);
+        
+    }
+    catch(e){
+        alert(e);
+    }
+    select.onchange = function(e){
+        get_all_submissions(this.value);
+    }    
+    
+}
+
+
+async function get_all_submissions(form_id) {
 
     try {
         var response = await
-        fetch('https://api.formx.stream/api/submissions/36', {
+        fetch('https://api.formx.stream/api/submissions/'+ form_id, {
             method: 'GET',
             headers: {
                 'Authorization': 'jwt ' + token,
@@ -106,7 +147,6 @@ async function get_all_submissions() {
         var json_response = await
         response.json()
         sub_data = json_response.data;
-        console.log(sub_data);
 
         sub_data.forEach(function (el) {
 
@@ -115,9 +155,6 @@ async function get_all_submissions() {
         
         var tableheading = Object.keys(tablebody[0]);
         tableheading.push("view");
-
-    console.log(tablebody);
-    console.log(tableheading);
         buildTable();
 
         for (i = 0; i < sub_data.length; i++) {
@@ -155,7 +192,6 @@ async function get_all_submissions() {
             }
             var btn = document.createElement('button');
             btn.innerText = "View";
-            console.log(sub_data);
             btn.id=sub_data[index].id;
             btn.className += "btn btn-primary"
             btn.setAttribute("data-target", "#myModal");
@@ -182,4 +218,4 @@ function  datatable() {
 
 }
 
-get_all_submissions();
+select_project();
